dc774a56d6c5085291d96ea0644ba8ac440b1eb3
Eduardo Patrocinio <epatro@gmail.com>
2016-10-06 09:18:40 -0300

    Add a sleep to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 . ./datapower-docker.cfg
 echo Using image: $DOCKER_IMAGE
 docker-compose up -d
+sleep 20
 URL=`docker ps | grep datapower | awk '{print $10}' | cut -d'-' -f1`
 echo Your DataPower Gateway is running at https://$URL. Log in using admin/admin
 
